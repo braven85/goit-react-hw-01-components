299f4ab6c419b1b6bdc523a3ea52dd76939c14af
Radek <radekie@wp.pl>
2022-02-05 19:40:08 +0100

    zrobione v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css';
+
 import Profile from './Profile/Profile';
 import user from './Profile/user.json';
+
 import Statistics from './Statistics/Statistics';
 import stats from './Statistics/data.json';
+
 import FriendList from './FriendList/FriendList';
 import friends from './FriendList/friends.json';
+
+import TransactionHistory from './Transactions/TransactionHistory';
+import items from './Transactions/transactions.json';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <a href='https://github.com/braven85/goit-react-hw-01-components'><h1>Oryginalny kod projektu</h1></a>
       {/* Zadanie 1 - Profil w sieci społecznościowej */}
       <Profile user={user} />
       {/* Zadanie 2 - Sekcja statystyki */}
@@ -17,6 +23,8 @@ function App() {
       <Statistics title="Upload stats" stats={stats} />
       {/* Zadanie 3 - Lista znajomych */}
       <FriendList friends={friends} />
+      {/* Zadanie 4 - Historia transakcji */}
+      <TransactionHistory items={items} />
     </div>
   );
 }
